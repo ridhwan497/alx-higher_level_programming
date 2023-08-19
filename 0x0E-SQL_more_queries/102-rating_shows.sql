8baf067d86071cd43620a3d83bc966e8014e4df7
ridhwan497 <ridubangire@gmail.com>
2023-08-18 19:19:23 -0700

    fix task

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,17 +1,7 @@
--- Lists all shows without the comedy genre in the database hbtn_0d_tvshows.
-SELECT DISTINCT `title`
+-- Lists all shows without the comedy genre in hbtn_0d_tvshows.
+SELECT `title`, SUM(`rate`) AS `rating`
   FROM `tv_shows` AS t
-       LEFT JOIN `tv_show_genres` AS s
-       ON s.`show_id` = t.`id`
-       LEFT JOIN `tv_genres` AS g
-       ON g.`id` = s.`genre_id`
-       WHERE t.`title` NOT IN
-             (SELECT `title`
-                FROM `tv_shows` AS t
-	             INNER JOIN `tv_show_genres` AS s
-		     ON s.`show_id` = t.`id`
-		     INNER JOIN `tv_genres` AS g
-		     ON g.`id` = s.`genre_id`
-		     WHERE g.`name` = "Comedy")
- ORDER BY `title`;
-
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
