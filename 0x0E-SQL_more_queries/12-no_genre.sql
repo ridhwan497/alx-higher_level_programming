12a8b260604ea38aa281e24c89e9a6a3459fc1c1
ridhwan497 <ridubangire@gmail.com>
2023-08-18 19:14:52 -0700

    fix task

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,7 @@
--- Import the database dump of hbtn_0d_tvshows
-SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s LEFT JOIN `tv_show_genres` AS g
-       ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id`;
-
+-- Import the database of hbtn_0d_tvshows
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ ORDER BY s.`title`, g.`genre_id`;
