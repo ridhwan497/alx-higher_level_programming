243ddf1c3ec0dff2c7f462af9783e42a3580e833
ridhwan497 <ridubangire@gmail.com>
2023-08-18 19:11:00 -0700

    fix task

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,19 +1,7 @@
 -- Lists all genres of the database hbtn_0d_tvshows
-SELECT DISTINCT `name`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`genre_id`
-
-       INNER JOIN `tv_shows` AS t
-       ON s.`show_id` = t.`id`
-       WHERE g.`name` NOT IN
-             (SELECT `name`
-                FROM `tv_genres` AS g
-	             INNER JOIN `tv_show_genres` AS s
-		     ON g.`id` = s.`genre_id`
-
-		     INNER JOIN `tv_shows` AS t
-		     ON s.`show_id` = t.`id`
-		     WHERE t.`title` = "Dexter")
- ORDER BY g.`name`;
-
+-- Records are sorted by ascending
+SELECT x.`title`, y.`genre_id`
+  FROM `tv_shows` AS x
+        INNER JOIN `tv_show_genres` AS y
+	ON x.`id` = y.`show_id`
+ ORDER BY x.`title`, y.`genre_id`;
